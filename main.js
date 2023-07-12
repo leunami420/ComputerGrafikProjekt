d429b0ec86c85528a40dd332a205a2c8948fb832
leunami420 <marconich311@gmail.com>
2023-07-12 16:14:52 +0200

    added simple spotlights and model updates

--- main.js
+++ main.js
@@ -4,26 +4,75 @@ import {
   Scene,
   WebGLRenderer,
   AmbientLight,
+  MeshPhongMaterial,
+  DirectionalLight,
+  DirectionalLightHelper,
+  SpotLight,
+  SpotLightHelper,
+  RectAreaLight,
+  
 } from 'three';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+import { RectAreaLightHelper } from 'three/addons/helpers/RectAreaLightHelper.js';
+import { RectAreaLightUniformsLib } from "three/examples/jsm/lights/RectAreaLightUniformsLib.js";
 
 async function init() {
   const container = document.querySelector('#scene-container');
   const scene = new Scene();
-  scene.background = new Color('black');
+  scene.background = new Color('blue');
+
 
   // Add light
-  const light = new AmbientLight(0xffffff);
+  const light = new AmbientLight(0xffffff,0.2);
   scene.add(light);
+
+  //spotlight
+  const dl = new SpotLight(0xffffff, 10)
+  dl.shadowMapVisible = true;
+  dl.position.set(280,290,30)
+  dl.target.position.set( 280, 0, 30 );
+  dl.distance = 300;
+  const dlHelper = new SpotLightHelper(dl,3)
+  scene.add(dl)
+  //scene.add(dlHelper)
+
+  //Plakatlight1
+  const pl1 = new SpotLight(0xffffff, 10)
+  pl1.position.set(-186,270,95)
+  pl1.target.position.set( -178, 0, 95 );
+  pl1.distance = 300;
+  const pl1Helper = new SpotLightHelper(pl1,3)
+  scene.add(pl1)
+  //scene.add(pl1Helper)
+
+  //Plakatlight2
+  const pl2 = new SpotLight(0xffffff, 10)
+  pl2.position.set(-170,270,95)
+  pl2.target.position.set( -178, 0, 95 );
+  pl2.distance = 300;
+  const pl2Helper = new SpotLightHelper(pl2,3)
+  scene.add(pl2)
+  //scene.add(pl2Helper)
+
+  //RectPlakatLight
+  //RectAreaLightUniformsLib.init();
+
+  //const pl1 = new RectAreaLight(0xffffff, 100,130,220)
+  //pl1.position.set(-178,155,95)
+  //pl1.rotation.y = (3 * Math.PI) / 2; // Rotate the light by 90 degrees
+  //const pl1Helper = new RectAreaLightHelper(pl1)
+  //scene.add(pl1)
+  //scene.add(pl1Helper)
 
   // Camera
   const camera = new PerspectiveCamera(45, container.clientWidth / container.clientHeight, 100, 200000);
-  camera.position.set(0, 2, 100);
+  camera.position.set(0, 300, 1000);
 
   // Render stuff
   const renderer = new WebGLRenderer();
+  renderer.shadowMapEnabled = true;
   renderer.setSize(container.clientWidth, container.clientHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
   container.appendChild(renderer.domElement);
@@ -40,7 +89,6 @@ async function init() {
     (object) => {
 
       scene.add(object);
-      fixTransparentTextures(object);
 
     },
     (xhr) => {
@@ -51,14 +99,7 @@ async function init() {
     }
   );
 
-  // Function to fix transparent textures
-  function fixTransparentTextures(object) {
-    object.traverse((child) => {
-      if (child.isMesh && child.material && child.material.transparent) {
-        child.material.depthWrite = false;
-      }
-    });
-  }
+
 
   // Render function
   function render() {
